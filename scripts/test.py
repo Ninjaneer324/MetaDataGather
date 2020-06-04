19ead776d121b181b6e9ba62dba5f2deab036898
Elaine Mo <kakashiandflute@gmail.com>
2020-06-04 01:02:08 -0500

    Why is it still returning Error 400

--- scripts/test.py
+++ scripts/test.py
@@ -1,1 +1,10 @@
-print("Hello World")
+import requests
+import json
+
+url = "https://api-na.hosted.exlibrisgroup.com/primo/v1/search"
+apikey = "l8xx76578ee844b74663a3cbe3b6bb3dd316"
+
+response = requests.get(url, params={"vid":"API_GUEST_INST:API_GUEST_INST", "tab":"LibraryCatalog","scope":"MyInstitution",
+"q":"creator,contains,elia zafrani,AND;any,contains,cloud computing", "qInclude":"facet_tlevel,include,online_resources|,|facet_rtype,exact,books", 
+"lang":"en", "inst":"API_GUEST_INST", "apikey":apikey})
+print(response.json())
