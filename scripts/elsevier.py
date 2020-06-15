cdbdc1935f9b2bc296bb85c5fa9f122437b9761f
Ninjaneer324 <kakashiandflute@gmail.com>
2020-06-15 16:26:00 -0500

    Back to science direct

--- scripts/elsevier.py
+++ scripts/elsevier.py
@@ -1,19 +1,11 @@
 import requests
 apiKey = "bbcd5fe7831eb12082993dcbaaa6d72c"
-url = "https://api.elsevier.com/content/search/scopus"
+url = "https://api.elsevier.com/content/search/sciencedirect"
 query = ""
 #default unavailable to have all access; need to email; limit in count number varies
 periodic_table = {"aluminum":"Al"}
 for i in periodic_table:
     query = "(" + i + " OR " + periodic_table[i] + ") AND (precipitat* OR age)"
-    response = requests.get(url, params={"httpAccept":"application/json","apiKey":apiKey, "query":query, "count":25})
+    response = requests.get(url, params={"httpAccept":"application/json","apiKey":apiKey, "query":query, "count":100})
     results = response.json()['search-results']['entry']
-    abstracts = {}
-    for i in results:
-        colon = i['dc:identifier'].find(':')
-        scopus_id = i['dc:identifier'][colon + 1:]
-        scopus_abstract = "https://api.elsevier.com/content/abstract/scopus_id/" + scopus_id
-        res_l = requests.get(scopus_abstract, params={"httpAccept":"application/json","apiKey":apiKey,"view":"META"})
-        result_l = res_l.json()
-        print(result_l)
-        break
+    
