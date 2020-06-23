39857ac53e197ad0a6eae055329c4c3b9e685e03
Ninjaneer324 <kakashiandflute@gmail.com>
2020-06-23 11:17:30 -0500

    Change query variable to match format

--- scripts/elsevier.py
+++ scripts/elsevier.py
@@ -16,7 +16,8 @@ excel_workbook = xlsxwriter.Workbook('Elsevier.xlsx')
 worksheet = excel_workbook.add_worksheet()
 #First 2 rows will detail what query format I applied
 worksheet.write(0,0,"Query Format")
-worksheet.write(0, 1, "(<element> OR <element_symbol>) AND ((precipitat* OR age) harden*)")
+worksheet.write(0, 1, "(((<base_element> OR <symbol>) AND (<alloy> OR <symbol>)) AND (precipitat* AND "+
+"(age* OR transform* OR microscop*))) NOT (aqueous OR bio* OR disease*)")
 
 #query portion
 row = 2
