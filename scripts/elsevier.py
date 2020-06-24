7d88369d1e1cc4cd23094878d34efd83c42a130b
Ninjaneer324 <kakashiandflute@gmail.com>
2020-06-24 13:20:28 -0500

    Formatting complete and spaces added

--- scripts/elsevier.py
+++ scripts/elsevier.py
@@ -11,6 +11,8 @@ access_token = ""
 url = "https://api.elsevier.com/content/ev/results"
 query = ""
 #The list of elements/alloys we intend to query
+
+
 periodic_table = {}
 periodic_wb = xlrd.open_workbook("/home/ninjaneer324/Documents/MetaDataGather/Period LIST.xlsx")
 sheet = periodic_wb.sheet_by_index(0)
@@ -20,6 +22,8 @@ for i in range(1, 96):
     contents['alloys'] = sheet.cell_value(i,3)
     periodic_table[sheet.cell_value(i, 2)] = contents
 #will this handle alloys with different names such as Nichrome or steel?
+
+
 #open exccel workbook
 excel_workbook = xlsxwriter.Workbook('Elsevier.xlsx')
 #add worksheet to workbook
@@ -28,6 +32,8 @@ worksheet = excel_workbook.add_worksheet()
 worksheet.write(0,0,"Query Format")
 worksheet.write(0, 1, "(((<base_element> OR <symbol>) AND (<alloy> OR <symbol>)) AND (precipitat* AND "+
 "(age* OR transform* OR microscop*))) NOT (aqueous OR bio* OR disease*)")
+
+
 #query portion
 row = 2
 #will hold metadata that will say what information that might be missing from each query
@@ -40,9 +46,8 @@ for elem in periodic_table:
     alloys = periodic_table[elem]['alloys'].split(', ')
     for a in alloys:
         query = "((("+periodic_table[elem]['name']+" OR "+elem+") AND ("+periodic_table[a]['name']+" OR "+a+")) AND (precipitat* AND "+"(age* OR transform* OR microscop*))) NOT (aqueous OR bio* OR disease*)"
-        print(query)
         
-        '''#requests for search results
+        #requests for search results
         response = requests.get(url, params={"httpAccept":"application/json","apiKey":apiKey, "access_token":access_token,"query":query})
         results = response.json()['search-results']['entry']
         #writes what element is currently being queried into worksheet
