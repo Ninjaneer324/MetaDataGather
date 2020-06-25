d6b984bfc779b9e277afde73d8c7d51362eabde7
Ninjaneer324 <kakashiandflute@gmail.com>
2020-06-25 11:42:05 -0500

    Changed script format

--- scripts/elsevier.py
+++ scripts/elsevier.py
@@ -32,10 +32,14 @@ worksheet = excel_workbook.add_worksheet()
 worksheet.write(0,0,"Query Format")
 worksheet.write(0, 1, "(((<base_element> OR <symbol>) AND (<alloy> OR <symbol>)) AND (precipitat* AND "+
 "(age* OR transform* OR microscop*))) NOT (aqueous OR bio* OR disease*)")
-
+worksheet.write(2,1,"DOI/ID")
+worksheet.write(2,2,"Title")
+worksheet.write(2,3,"Author")
+worksheet.write(2,4,"Cover Date")
+worksheet.write(2,5,"Load Date")
 
 #query portion
-row = 2
+row = 4
 #will hold metadata that will say what information that might be missing from each query
 for elem in periodic_table:
     #formats query for each element in the "periodic table"
@@ -46,51 +50,47 @@ for elem in periodic_table:
     alloys = periodic_table[elem]['alloys'].split(', ')
     for a in alloys:
         query = "((("+periodic_table[elem]['name']+" OR "+elem+") AND ("+periodic_table[a]['name']+" OR "+a+")) AND (precipitat* AND "+"(age* OR transform* OR microscop*))) NOT (aqueous OR bio* OR disease*)"
+        worksheet.write(row, 0, elem+"-"+a)
         print(query)
         #requests for search results
         response = requests.get(url, params={"httpAccept":"application/json","apiKey":apiKey, "access_token":access_token,"query":query})
         results = response.json()['search-results']['entry']
         #writes what element is currently being queried into worksheet
         worksheet.write(row, 0, elem + "-" +a)
-        worksheet.write(row + 1, 0, "Missing Information")
-        col = 1
         for r in results:
-            cell = {}
-            missing = []
             #checks that said meta data exists and writes it to the excel spread sheet cell if it does and 
             #to the missing information row if it doesn't
             if 'dc:identifier' in r:
                 id = r['dc:identifier']
-                cell['id'] = id
+                worksheet.write(row, 1, id)
             else:
-                missing.append('id')
+                worksheet.write(row,1,"Missing")
 
             if 'dc:title' in r:
                 title = r['dc:title']
-                cell['title'] = title
+                worksheet.write(row, 2, title)
             else:
-                missing.append('title')
+                worksheet.write(row, 2, "Missing")
         
             if 'dc:creator' in r:
                 creator = r['dc:creator']
-                cell['author'] = creator
+                worksheet.write(row, 3, creator)
             else:
-                missing.append('author')
+                missing.write(row, 3, "Missing")
             
             if 'prism:coverDate' in r:
-                cell['prism:coverDate'] = r['prism:coverDate']
+                coverDate = r['prism:coverDate']
+                worksheet.write(row, 4, coverDate)
             else:
-                missing.append('prism:coverDate')
+                worksheet.write(row,4,"Missing")
             
             if 'load-date' in r:
-                cell['load-date'] = r['load-date']
+                load_date = r['load-date']
+                worksheet.write(row, 5, load_date)
             else:
-                missing.append('load-date')
-            worksheet.write(row, col, str(cell))
-            worksheet.write(row + 1, col, str(missing))
-            col += 1
-        row+=2
-    row+=1
+                worksheet.write(row,5,"Missing")
+            row += 1
+        row+=1
 #close workbook'''
     print()
 excel_workbook.close()
