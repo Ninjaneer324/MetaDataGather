c49fd7c8bc059c537f6bc6fbcd7d7b4275653719
Ninjaneer324 <kakashiandflute@gmail.com>
2020-07-09 12:08:50 -0500

    Science Direct needs to adjust query

--- scripts/elsevier-ev.py
+++ scripts/elsevier-ev.py
@@ -7,9 +7,9 @@ from urllib.parse import unquote
 apiKey = "bbcd5fe7831eb12082993dcbaaa6d72c"
 #or is it the insttoken?
 #API Documentation for Engineering Village search: https://dev.elsevier.com/documentation/EngineeringVillageAPI.wadl
-access_token = "4f3d2a4d46c51cbb68e83cf0b7150f45"
+inst_token = "4f3d2a4d46c51cbb68e83cf0b7150f45"
 #endpoint for search
-headers = {"Accept":"application/json","X-ELS-APIKey":apiKey, "X-ELS-Insttoken":access_token}
+headers = {"Accept":"application/json","X-ELS-APIKey":apiKey, "X-ELS-Insttoken":inst_token}
 url = "https://api.elsevier.com/content/ev/results"
 #The list of elements/alloys we intend to query
 
--- scripts/elsevier-sd.py
+++ scripts/elsevier-sd.py
@@ -7,9 +7,9 @@ import time
 apiKey = "bbcd5fe7831eb12082993dcbaaa6d72c"
 #or is it the insttoken?
 #API Documentation for Engineering Village search: https://dev.elsevier.com/documentation/EngineeringVillageAPI.wadl
-access_token = "4f3d2a4d46c51cbb68e83cf0b7150f45"
+inst_token = "4f3d2a4d46c51cbb68e83cf0b7150f45"
 #endpoint for search
-headers = {"Accept":"application/json","X-ELS-APIKey":apiKey, "X-ELS-Insttoken":access_token}
+headers = {"Accept":"application/json","X-ELS-APIKey":apiKey, "X-ELS-Insttoken":inst_token}
 url = "https://api.elsevier.com/content/search/sciencedirect"
 #The list of elements/alloys we intend to query
 
@@ -54,11 +54,11 @@ for elem in periodic_table:
         alloy_element = periodic_table[a]['name']
         alloy_symbol = a
         #query = "("+base_element+" OR "+base_symbol+") AND ("+alloy_element+" OR "+alloy_symbol+") AND (age* OR aging OR precipitat*) AND (phase* OR hardness OR hardening OR tensile OR microsc* or SEM OR TEM OR diffract* OR dilatom* OR (mech* AND (prop* OR response)))"
-        query = "(("+periodic_table[a]['name']+" OR "+a+") AND (precipitat* AND "+"(age* OR transform* OR microscop*)))"
+        query = "(("+periodic_table[a]['name']+" OR "+a+") AND (precipitate AND "+"(age OR transform OR microscope)))"
         worksheet.write(row, 0, elem+"-"+a)
         print(query)
         #requests for search results
-        response = requests.get(url, headers=headers,params={"query":query,"count":200}) #engineering village doesn't have count
+        response = requests.get(url, headers=headers,params={"query":query,"count":200,"sort":"relevance"}) #engineering village doesn't have count
         
         if response.status_code != 200:
             print("Error: HTTP", response.status_code)
@@ -68,39 +68,44 @@ for elem in periodic_table:
         print(response.status_code)
         #writes what element is currently being queried into worksheet
         worksheet.write(row + 1, 0, "Total: " + str(results['search-results']['opensearch:totalResults']))
-        for r in results['search-results']['entry']:
-            #checks that said meta data exists and writes it to the excel spread sheet cell if it does and 
-            #to the missing information row if it doesn't
-            if 'dc:identifier' in r:
-                id = r['dc:identifier']
-                worksheet.write(row, 1, id)
-            else:
-                worksheet.write(row,1,"Missing")
-            if 'dc:title' in r:
-                title = r['dc:title']
-                worksheet.write(row, 2, title)
-            else:
-                worksheet.write(row, 2, "Missing")
-            
-            if 'dc:creator' in r:
-                creator = r['dc:creator']
-                worksheet.write(row, 3, creator)
-            else:
-                missing.write(row, 3, "Missing")
+        print(results)
+        exit()
+        if 'entry' in results['search-results']:
+            for r in results['search-results']['entry']:
+                #checks that said meta data exists and writes it to the excel spread sheet cell if it does and 
+                #to the missing information row if it doesn't
+                if 'dc:identifier' in r:
+                    id = r['dc:identifier']
+                    worksheet.write(row, 1, id)
+                else:
+                    worksheet.write(row,1,"Missing")
+                if 'dc:title' in r:
+                    title = r['dc:title']
+                    worksheet.write(row, 2, title)
+                else:
+                    worksheet.write(row, 2, "Missing")
                 
-            if 'prism:coverDate' in r:
-                coverDate = r['prism:coverDate']
-                worksheet.write(row, 4, coverDate)
-            else:
-                worksheet.write(row,4,"Missing")
-                
-            if 'load-date' in r:
-                load_date = r['load-date']
-                worksheet.write(row, 5, load_date)
-            else:
-                worksheet.write(row,5,"Missing")
+                if 'dc:creator' in r:
+                    creator = r['dc:creator']
+                    worksheet.write(row, 3, creator)
+                else:
+                    missing.write(row, 3, "Missing")
+                    
+                if 'prism:coverDate' in r:
+                    coverDate = r['prism:coverDate']
+                    worksheet.write(row, 4, coverDate)
+                else:
+                    worksheet.write(row,4,"Missing")
+                    
+                if 'load-date' in r:
+                    load_date = r['load-date']
+                    worksheet.write(row, 5, load_date)
+                else:
+                    worksheet.write(row,5,"Missing")
+                row += 1
             row += 1
+        else:
+            row += 3
         time.sleep(2)
-        row += 1
 print("Closing Workbook...")
 excel_workbook.close()
