267e9730e561ff9bfb4987b1f615de3ee94ff35e
Ninjaneer324 <kakashiandflute@gmail.com>
2020-07-12 20:42:57 -0500

    Figure out gensim sooner or later

--- scripts/gensim-tester.py
+++ scripts/gensim-tester.py
@@ -1,4 +1,4 @@
-'''import gensim
+import gensim
 from gensim import corpora
 from pprint import pprint
 from gensim.utils import simple_preprocess
@@ -18,14 +18,4 @@ print(dictionary.token2id)
 #>  'as': 43,
 #>  'at': 24,
 #>   ...
-#> }'''
-import requests
-apiKey = "bbcd5fe7831eb12082993dcbaaa6d72c"
-
-inst_token = "4f3d2a4d46c51cbb68e83cf0b7150f45"
-
-url = "https://api.elsevier.com/content/ev/records"
-headers = {"Accept":"application/json","X-ELS-APIKey":apiKey, "X-ELS-Insttoken":inst_token}
-r = requests.get(url, headers=headers,params={"docId":"cpx_6e3d601232e2239f5M5bcd2061377553"})
-results = r.json()
-print(results['PAGE']['PAGE-RESULTS']['PAGE-ENTRY'][0]['EI-DOCUMENT']['DOCUMENTPROPERTIES']['AB'])
+#> }
