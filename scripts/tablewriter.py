e14098b6e70e5eecfdb736a29eee9dcc281dbe2b
Ninjaneer324 <kakashiandflute@gmail.com>
2020-07-22 10:44:58 -0500

    script written

--- scripts/tablewriter.py
+++ scripts/tablewriter.py
@@ -2,7 +2,9 @@ import xlsxwriter
 import xlrd
 import requests
 import time
+from dateutil.parser import parse
 from math import ceil
+import re
 
 apiKey = "bbcd5fe7831eb12082993dcbaaa6d72c"
 #or is it the insttoken?
@@ -19,6 +21,7 @@ for i in range(1, 96):
     contents = {}
     contents['name'] = sheet.cell_value(i, 1)
     contents['alloys'] = sheet.cell_value(i,3)
+    contents['pos'] = i
     periodic_table[sheet.cell_value(i, 2)] = contents
 
 nxn_table = xlsxwriter.Workbook('FinalProduct.xlsx')
@@ -37,9 +40,12 @@ def searchBaseAlloy(base_n, base_s, alloy_n, alloy_s):
     row = 0
     worksheet.write(row, 0, "Query Format")
     f_mat = "\"{0} alloys\" AND \"-*{3}\" AND (age* OR aging OR precipitat*) AND (hardness OR hardening OR harden* OR strength*) AND (phase* OR tensile OR microsc* or SEM OR TEM OR diffract* OR dilatom* OR (mech* AND (prop* OR response))) NOT (biol* OR diseas* OR cancer OR aqueous* OR ceramic OR \" Fe-*\" OR steel OR \" Al-*\" OR \" Mg-*\" OR \"0-9+\" OR \"IV*\" OR \"VI*\")"
+    if base_n == alloy_n:
+        f_mat = "\"pure "+base_n+"\" AND (age* OR aging OR precipitat*) AND (hardness OR hardening OR harden* OR strength*) AND (phase* OR tensile OR microsc* or SEM OR TEM OR diffract* OR dilatom* OR (mech* AND (prop* OR response))) NOT (biol* OR diseas* OR cancer OR aqueous* OR ceramic OR \" Fe-*\" OR steel OR \" Al-*\" OR \" Mg-*\" OR \"0-9+\" OR \"IV*\" OR \"VI*\")"
     worksheet.write(row, 1, f_mat)
     row = 2
     query = f_mat.format(base_n, base_s, alloy_n, alloy_s)
+    print(query)
     response = requests.get(url, headers=headers,params={"query":query})
     print("Page 1",response.status_code)
     if response.status_code != 200:
@@ -107,7 +113,7 @@ def searchBaseAlloy(base_n, base_s, alloy_n, alloy_s):
 
 
         #next 8 pages if any
-        pages = min(8, ceil(total_results / 100))
+        '''pages = min(8, ceil(total_results / 100))
         for i in range(1, pages):
             response = requests.get(url, headers=headers,params={"query":query,"offset":i,"pageSize":100,"database":"c","sortField":"relevance"}) #engineering village doesn't have count
             print("Page",i + 1,response.status_code)
@@ -168,10 +174,42 @@ def searchBaseAlloy(base_n, base_s, alloy_n, alloy_s):
                     ids['DOC-ID'] = id
                     worksheet.write(row, 1, str(ids))
                 row += 1
-            time.sleep(2)
+            time.sleep(2)'''
+        total_results -= exclude
     else:
         time.sleep(2)
+    worksheet.write(3, 0, total_results)
     workbook.close()
+    return total_results
 
+workbook_array = [["" for i in range(1, 96)] for j in range(1, 96)]
 
-searchBaseAlloy("Molybdenum", "Mo", "Rhenium", "Re")
+for i in periodic_table:
+    for j in periodic_table:
+        cell_str = ""
+        if i == j:
+            continue
+        total = searchBaseAlloy(periodic_table[i]['name'], i, periodic_table[j]['name'], j)
+        filename = "Mo-"+i+".xlsx"
+        if total != 0:
+            read_workbook = xlrd.open_workbook(filename)
+            sheet = read_workbook.sheet_by_index(0)
+            len_cnt = 0
+            for row in range(2, sheet.nrows):
+                date = sheet.cell_value(row, 4)
+                year = None
+                years = re.findall(r"[0-9]{4,7}(?![0-9])", date)
+                if len(years) != 0:
+                    year = years[0] #figure this out 
+                authorlist = sheet.cell_value(row, 3)
+                comma = authorlist.find(',')
+                author = authorlist[0:comma]
+                cell_str += str(year) + author
+                len_cnt += 1
+                if row != sheet.nrows - 1 and len_cnt < 10:
+                    cell_str += ";"
+                else:
+                    break
+        workbook_array[periodic_table["Mo"]["pos"] - 1][periodic_table[i]["pos"] - 1] = cell_str
+
+nxn_table.close()
