639bf85636666f6ea2f25dab2339031066081338
Ninjaneer324 <kakashiandflute@gmail.com>
2020-07-30 10:56:32 -0500

    For FirstDatabase, filter was rewritten

--- scripts/filter.py
+++ scripts/filter.py
@@ -6,33 +6,32 @@ from math import ceil
 from re import search
 from urllib.parse import unquote
 
-no_element = xlrd.open_workbook("NoElement.xlsx")
+no_element = xlrd.open_workbook("FirstDataBase01.xlsx")
 no_element_sheet = no_element.sheet_by_index(0)
-filtered = xlsxwriter.Workbook("NoElement-filtered.xlsx")
+filtered = xlsxwriter.Workbook("FirstDataBase02.xlsx")
 fil_sheet = filtered.add_worksheet()
 
 uniques = {}
 
-for r in range(2, no_element_sheet.nrows):
-    title = no_element_sheet.cell_value(r, 2)
+for r in range(1, no_element_sheet.nrows):
+    title = str(no_element_sheet.cell_value(r, 2))
     if title.lower() not in uniques:
         content = {}
-        content['ids'] = no_element_sheet.cell_value(r, 1)
+        content['ids'] = no_element_sheet.cell_value(r, 0)
+        content['year'] = no_element_sheet.cell_value(r, 1)
         content['title'] = title
         content['author'] = no_element_sheet.cell_value(r, 3)
-        content['date'] = no_element_sheet.cell_value(r, 4)
         uniques[title.lower()] = content
-
-fil_sheet.write(0, 0, "Query Format")
-fil_sheet.write(0, 1, no_element_sheet.cell_value(0, 1))
-fil_sheet.write(2, 0, no_element_sheet.cell_value(2, 0))
-fil_sheet.write(3, 0, no_element_sheet.cell_value(3, 0))
 del no_element
-row = 2
+fil_sheet.write(0, 0, "DOI")
+fil_sheet.write(0, 1, "Year")
+fil_sheet.write(0, 2, "Title")
+fil_sheet.write(0, 3, "Author")
+row = 1
 for i in uniques:
-    fil_sheet.write(row, 1, uniques[i]['ids'])
+    fil_sheet.write(row, 0, uniques[i]['ids'])
+    fil_sheet.write(row, 1, uniques[i]['year'])
     fil_sheet.write(row, 2, uniques[i]['title'])
     fil_sheet.write(row, 3, uniques[i]['author'])
-    fil_sheet.write(row, 4, uniques[i]['date'])
     row += 1
 filtered.close()
