d78379d0c17df6ac3023a49560b871509a362377
Ninjaneer324 <kakashiandflute@gmail.com>
2020-08-01 01:22:42 -0500

    Check filter.py works with precip hardening

--- scripts/filter.py
+++ scripts/filter.py
@@ -11,7 +11,47 @@ no_element_sheet = no_element.sheet_by_index(0)
 filtered = xlsxwriter.Workbook("FirstDataBase02.xlsx")
 fil_sheet = filtered.add_worksheet()
 
-uniques = {}
+best_terms = []
+with open("best_terms.txt", "r") as file:
+    for line in file:
+        res = line.strip().replace("*", ".*")
+        best_terms.append(res)
+
+good_terms = []
+with open("good_terms.txt", "r") as file:
+    for line in file:
+        res = line.strip().replace("*", ".*")
+        good_terms.append(res)
+
+margin_good_terms = []
+with open("margin_good_terms.txt", "r") as file:
+    for line in file:
+        res = line.strip().replace("*", ".*")
+        margin_good_terms.append(res)
+
+neutral_terms = []
+with open("neutral_terms.txt", "r") as file:
+    for line in file:
+        res = line.strip().replace("*", ".*")
+        neutral_terms.append(res)
+
+margin_bad_terms = []
+with open("margin_bad_terms.txt", "r") as file:
+    for line in file:
+        res = line.strip().replace("*", ".*")
+        margin_bad_terms.append(res)
+
+bad_terms = []
+with open("bad_terms.txt", "r") as file:
+    for line in file:
+        res = line.strip().replace("*", ".*")
+        bad_terms.append(res)
+
+unpromising_terms = []
+with open("unpromising_terms.txt", "r") as file:
+    for line in file:
+        res = line.strip().replace("*", ".*")
+        unpromising_terms.append(res)
 
 periodic_table = {}
 periodic_wb = xlrd.open_workbook("Periodic-Table.xlsx")
@@ -52,17 +92,55 @@ def containsElement(input_str=""):
         if 'symbol' in periodic_table[i]:
             base = periodic_table[i]['symbol'] + r"-.*"
             alloy = r"-.*" + periodic_table[i]['symbol']
-            alloy_names = None
+        
+        alloy_names = None
         if 'alloy_names' in periodic_table[i]:
             alloy_names = periodic_table[i]['alloy_names']
         if (i.lower() in input_str.lower()) or search(base, input_str) or search(alloy, input_str) or (alloy_names is not None and any(item in input_str for item in alloy_names)):
             return True
     return False
 
+def totalScore(input_str=""):
+    score = 0
+    for i in best_terms:
+        reg = r""+i
+        if search(reg, input_str):
+            score += 10
+    
+    for i in good_terms:
+        reg = r""+i
+        if search(reg, input_str):
+            score += 3
+
+    for i in margin_good_terms:
+        reg = r""+i
+        if search(reg, input_str):
+            score += 1
+    
+    for i in margin_bad_terms:
+        reg = r""+i
+        if search(reg, input_str):
+            score -= 1
+
+    for i in bad_terms:
+        reg = r""+i
+        if search(reg, input_str):
+            score -= 3
+    
+    for i in unpromising_terms:
+        reg = r""+i
+        if search(reg, input_str):
+            score -= 10
+    
+    return score
+
+uniques = {}
+
 for r in range(1, no_element_sheet.nrows):
     title = str(no_element_sheet.cell_value(r, 5))
     abstract = str(no_element_sheet.cell_value(r, 2))
-    if title.lower() not in uniques and (containsElement(title) or containsElement(abstract)):
+    keywords = str(no_element_sheet.cell_value(r, 6))
+    if title.lower() not in uniques and (containsElement(title) or containsElement(abstract)) and ((totalScore(title) + totalScore(abstract) +totalScore(keywords)) > 0):
         content = {}
         content['reference-type'] = no_element_sheet.cell_value(r,0)
         content['record-number'] = no_element_sheet.cell_value(r, 1)
@@ -70,10 +148,13 @@ for r in range(1, no_element_sheet.nrows):
         content['author'] = no_element_sheet.cell_value(r, 3)
         content['year'] = no_element_sheet.cell_value(r, 4)
         content['title'] = title
-        content['keywords'] = no_element_sheet.cell_value(r, 6)
-        content['label'] = no_element_sheet.cell_value(r, 7)
-        content['lanl-style'] = no_element_sheet.cell_value(r, 8)
+        content['keywords'] = keywords
+        content['journal'] = no_element_sheet.cell_value(r, 7)
+        content['label'] = no_element_sheet.cell_value(r, 8)
+        content['lanl-style'] = no_element_sheet.cell_value(r, 9)
         uniques[title.lower()] = content
+        
+
 del no_element
 fil_sheet.write(0, 0, "Reference Type")
 fil_sheet.write(0, 1, "Record Number")
@@ -82,8 +163,9 @@ fil_sheet.write(0, 3, "Author")
 fil_sheet.write(0, 4, "Title")
 fil_sheet.write(0, 5, "Abstract")
 fil_sheet.write(0, 6, "Keywords")
-fil_sheet.write(0, 7, "Label")
-fil_sheet.write(0, 8, "LANL Style")
+fil_sheet.write(0, 7, "Journal")
+fil_sheet.write(0, 8, "Label")
+fil_sheet.write(0, 9, "LANL Style")
 row = 1
 for i in uniques:
     fil_sheet.write(row, 0, uniques[i]['reference-type'])
@@ -93,7 +175,8 @@ for i in uniques:
     fil_sheet.write(row, 4, uniques[i]['title'])
     fil_sheet.write(row, 5, uniques[i]['abstract'])
     fil_sheet.write(row, 6, uniques[i]['keywords'])
-    fil_sheet.write(row, 7, uniques[i]['label'])
-    fil_sheet.write(row, 8, uniques[i]['lanl-style'])
+    fil_sheet.write(row, 7, uniques[i]['journal'])
+    fil_sheet.write(row, 8, uniques[i]['label'])
+    fil_sheet.write(row, 9, uniques[i]['lanl-style'])
     row += 1
 filtered.close()
