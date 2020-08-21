c3d47fdbde9ca30fb3d3ac310d51ec4e86231a12
Ninjaneer324 <kakashiandflute@gmail.com>
2020-08-20 23:56:07 -0500

    comments added

--- scripts/tester.py
+++ scripts/tester.py
@@ -2,5 +2,7 @@ reg = r"\s+Mo[\s-]*"
 reg_2 = r"-+[awt\.0-9%\s]*Cu[\s-]*"
 reg_3 = r"-+[awt\.0-9%\s]*Mg[\s-]*"
 import re
+
+#trying to ignore other words and look for Mo-Cu or Mo-4%w.t.Cu or Mo-4Cu etc.
 if re.search(reg, "hardening of meMo- and Cu as well as Mg---"):
     print("pass")
